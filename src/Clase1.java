ad7c38f5fbb3c5c379a56cc152e82b6f74c442ab
Carlos de Tena Bellmont <carlostena@centroafuera.es>
2020-06-29 23:28:57 +0200

    Initial commit

---
+++ src/Clase1.java
@@ -0,0 +1,11 @@
+public class Clase1 {
+
+    public int atributoInt1 = 100;
+
+    public void escribeHola(){
+        System.out.println("Hola");
+    }
+
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Clase1 myObjeto = new Clase1();
+        myObjeto.escribeHola();
+        System.out.println("Me gusta el número " + myObjeto.atributoInt1);
+    }
+}
